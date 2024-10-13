f70853cc87b6a156944a449c2171a645489ddc16
ErickJamesRubin <rubin.erick123@gmail.com>
2024-10-13 12:50:24 +0800

    Added createBarChar implementation in chart.js

--- charts/chart.js
+++ charts/chart.js
@@ -64,6 +64,38 @@ class LineChart extends ChartCreator {
     }
 }
 
+class BarChart extends ChartCreator {
+    constructor(dataUrl) {
+        super(dataUrl);
+        this.barCtx = document.getElementById('barChart');
+    }
+
+    createCharts() {
+        this.createBarChart();
+    }
+
+    createBarChart() {
+        new Chart(this.barCtx, {
+            type: 'bar',
+            data: {
+                labels: this.chartData.labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: this.chartData.data,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+}
+
 const lineChartCreator = new LineChart('data.json');
 lineChartCreator.init();
 
